030fbd112c00cd4ca32ff774eed0ee20b341514b
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-01-10 07:07:02 +0000

    wip

--- tests/test_create_release_notes.py
+++ tests/test_create_release_notes.py
@@ -2,6 +2,14 @@ import unittest
 from unittest.mock import patch
 from create_release_notes import create_release_notes
 
+test_commit = """
+    {"commit": {
+        "message": "Remove completion functions from GitAuthor"
+    },
+    "sha": "1292bf0e22c796e91cc3d6e24b544aece8c21f2a"
+    }
+"""
+
 
 def mocked_jira_get_issue(*args, **kwargs):
     if args[0] == "TEST-123":
@@ -49,44 +57,6 @@ class TestGetJiraDetails(unittest.TestCase):
         self.assertEqual(jira_details.components, ["Component1", "Component2"])
         self.assertEqual(jira_details.impact, "High")
         self.assertEqual(jira_details.business_service_impact, "Service Impact")
-
-    @patch("create_release_notes.create_release_notes.Jira")
-    def test_get_jira_details_no_user_story(self, mock_jira):
-        mock_jira.get_issue.side_effect = mocked_jira_get_issue
-
-        jira_details = create_release_notes.get_jira_details(mock_jira, "TEST-124")
-
-        self.assertEqual(jira_details.jira_title, "Test Summary")
-        self.assertEqual(jira_details.user_story, "can not find user story")
-        self.assertEqual(jira_details.components, ["Component1", "Component2"])
-        self.assertEqual(jira_details.impact, "High")
-        self.assertEqual(jira_details.business_service_impact, "Service Impact")
-
-    @patch("create_release_notes.create_release_notes.Jira")
-    def test_get_jira_details_no_impact(self, mock_jira):
-        mock_jira.get_issue.side_effect = mocked_jira_get_issue
-
-        jira_details = create_release_notes.get_jira_details(mock_jira, "TEST-125")
-
-        self.assertEqual(jira_details.jira_title, "Test Summary")
-        self.assertEqual(jira_details.user_story, "Test User Story")
-        self.assertEqual(jira_details.components, ["Component1", "Component2"])
-        self.assertEqual(jira_details.impact, "")
-        self.assertEqual(jira_details.business_service_impact, "Service Impact")
-
-    @patch("create_release_notes.create_release_notes.Jira")
-    def test_get_jira_details_exception(self, mock_jira):
-        mock_jira.get_issue.side_effect = mocked_jira_get_issue
-
-        jira_details = create_release_notes.get_jira_details(mock_jira, "TEST-126")
-
-        self.assertEqual(
-            jira_details.jira_title, "can not find jira ticket for TEST-126"
-        )
-        self.assertEqual(jira_details.user_story, "")
-        self.assertEqual(jira_details.components, [])
-        self.assertEqual(jira_details.impact, "")
-        self.assertEqual(jira_details.business_service_impact, "")
 
 
 if __name__ == "__main__":
