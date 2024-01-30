1b3e7da2b335f6bb0079d666a9edf9cc85e6ba41
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-01-30 10:42:54 +0000

    refactor tests

--- tests/test_create_release_notes.py
+++ tests/test_create_release_notes.py
@@ -10,6 +10,26 @@ from tests.common import (
     mocked_get_tags,
 )
 
+param_list = [
+    ("normal release notes", False, None, 0, 0, expected_release_notes),
+    (
+        "rc release notes with release url",
+        True,
+        "https://github.com/NHSDigital/prescriptionsforpatients/actions/runs/7692810696",
+        0,
+        0,
+        expected_rc_release_notes_with_release_run_link,
+    ),
+    (
+        "rc release notes without release url",
+        True,
+        None,
+        0,
+        0,
+        expected_rc_release_notes_with_no_release_run_link,
+    ),
+]
+
 
 class TestGetJiraDetails(unittest.TestCase):
     @patch("create_release_notes.create_release_notes.Jira")
@@ -18,74 +38,36 @@ class TestGetJiraDetails(unittest.TestCase):
 
         tags = mocked_get_tags()
         diff = mocked_compare()
+        for (
+            scenario,
+            create_release_candidate,
+            release_url,
+            edit_issue_call_count,
+            issue_transition_call_count,
+            expected_output,
+        ) in param_list:
+            with self.subTest(
+                msg=scenario,
+            ):
+                release_notes = create_release_notes.create_release_notes(
+                    jira=mock_jira,
+                    current_tag="tag_1",
+                    target_tag="tag_3",
+                    target_environment="INT",
+                    product_name="EPS FHIR API",
+                    create_release_candidate=create_release_candidate,
+                    release_name="v1.0.442-beta ",
+                    release_url=release_url,
+                    diff=diff,
+                    tags=tags,
+                    repo_name="prescriptionsforpatients",
+                )
 
-        release_notes = create_release_notes.create_release_notes(
-            jira=mock_jira,
-            current_tag="tag_1",
-            target_tag="tag_3",
-            target_environment="INT",
-            product_name="EPS FHIR API",
-            create_release_candidate=False,
-            release_name="v1.0.442-beta ",
-            release_url="https://github.com/NHSDigital/prescriptionsforpatients/actions/runs/7692810696",
-            diff=diff,
-            tags=tags,
-            repo_name="prescriptionsforpatients",
-        )
-
-        self.assertEqual(release_notes, expected_release_notes)
-
-    @patch("create_release_notes.create_release_notes.Jira")
-    def test_create_rc_release_notes_with_run_link(self, mock_jira):
-        mock_jira.get_issue.side_effect = mocked_jira_get_issue
-
-        tags = mocked_get_tags()
-        diff = mocked_compare()
-
-        release_notes = create_release_notes.create_release_notes(
-            jira=mock_jira,
-            current_tag="tag_1",
-            target_tag="tag_3",
-            target_environment="INT",
-            product_name="EPS FHIR API",
-            create_release_candidate=True,
-            release_name="v1.0.442-beta ",
-            release_url="https://github.com/NHSDigital/prescriptionsforpatients/actions/runs/7692810696",
-            diff=diff,
-            tags=tags,
-            repo_name="prescriptionsforpatients",
-        )
-
-        self.assertEqual(release_notes, expected_rc_release_notes_with_release_run_link)
-        # self.assertEqual(mock_jira.edit_issue.call_count, 2)
-        # self.assertEqual(mock_jira.issue_transition.call_count, 2)
-
-    @patch("create_release_notes.create_release_notes.Jira")
-    def test_create_rc_release_notes_without_run_link(self, mock_jira):
-        mock_jira.get_issue.side_effect = mocked_jira_get_issue
-
-        tags = mocked_get_tags()
-        diff = mocked_compare()
-
-        release_notes = create_release_notes.create_release_notes(
-            jira=mock_jira,
-            current_tag="tag_1",
-            target_tag="tag_3",
-            target_environment="INT",
-            product_name="EPS FHIR API",
-            create_release_candidate=True,
-            release_url=None,
-            release_name="v1.0.442-beta ",
-            diff=diff,
-            tags=tags,
-            repo_name="prescriptionsforpatients",
-        )
-
-        self.assertEqual(
-            release_notes, expected_rc_release_notes_with_no_release_run_link
-        )
-        # self.assertEqual(mock_jira.edit_issue.call_count, 2)
-        # self.assertEqual(mock_jira.issue_transition.call_count, 2)
+                self.assertEqual(release_notes, expected_output)
+                self.assertEqual(mock_jira.edit_issue.call_count, edit_issue_call_count)
+                self.assertEqual(
+                    mock_jira.issue_transition.call_count, issue_transition_call_count
+                )
 
 
 if __name__ == "__main__":
